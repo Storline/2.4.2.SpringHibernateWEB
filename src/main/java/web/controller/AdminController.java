b556ee94eb5b95086536f05b6b4da93dd2b42069
Storline <pustigyn.a@gmail.com>
2021-04-06 00:40:06 +0300

    Test

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -5,13 +5,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import web.model.Role;
 import web.model.User;
 import web.service.UserService;
-
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @Controller
